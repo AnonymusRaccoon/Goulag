db5d16190c19ee007acdfb0586fde7bb6a5ac3d1
happy44300 <bilal.molli@gmail.com>
2018-10-30 19:08:18 +0100

    CONCRETE

---
+++ Assets/Script/RGB.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RGB : MonoBehaviour {
+    [SerializeField]
+    public float Speed = 1;
+ 
+    private Renderer rend;
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        rend.material.SetColor("_Color", HSBColor.ToColor(new HSBColor(Mathf.PingPong(Time.time * Speed, 1), 1, 1)));
+    }
+}
+ 
+    
+
+
+
